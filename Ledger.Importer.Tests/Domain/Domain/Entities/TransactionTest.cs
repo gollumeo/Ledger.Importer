9f185978c823d78e77f4ddc9d671ac6cfa7374a8
Golluméo <p.mauriello54@gmail.com>
2025-05-10 12:22:21 +0200

    test(transaction): add unit tests for Transaction entity
    Adds tests to ensure Transaction is created correctly with valid data.
    Includes validation for empty descriptions throwing the appropriate exception.

---
+++ Ledger.Importer.Tests/Domain/Domain/Entities/TransactionTest.cs
@@ -0,0 +1,29 @@
+﻿
+using FluentAssertions;
+using Ledger.Importer.Domain.Entities;
+using Ledger.Importer.Domain.Exceptions;
+using Ledger.Importer.Domain.ValueObjects;
+
+namespace Ledger.Importer.Tests.Domain.Domain.Entities;
+
+public class TransactionTest
+{
+    [Fact]
+    public void TransactionIsCreatedSuccessfullyWithValidData()
+    {
+        var date = new TransactionDate(DateTime.UtcNow);
+        var transaction = new Transaction("Uber Eats Paris", 29.90m, date);
+        
+        transaction.Description.Should().Be("Uber Eats Paris");
+        transaction.Amount.Should().Be(29.90m);
+        transaction.Date.Should().Be(date);
+    }
+    
+    [Fact]
+    public void ExceptionIsThrownWhenDescriptionIsEmpty()
+    {
+        var transactionConstruction = () => new Transaction("", 29.90m, new TransactionDate(DateTime.UtcNow));
+
+        transactionConstruction.Should().Throw<InvalidTransactionData>();
+    }
+}
