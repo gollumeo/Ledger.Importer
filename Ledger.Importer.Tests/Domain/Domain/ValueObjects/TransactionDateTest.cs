bffc6dc20f72499302ba4deca471c36fde9226d5
Golluméo <p.mauriello54@gmail.com>
2025-05-11 19:02:13 +0200

    test(valueobjects): add unit tests for TransactionDate
    Adds unit tests to validate the behavior of the TransactionDate value object.
    Tests cover valid creation, parsing from ISO and simplified formats, and invalid cases.
    Ensures robustness of date handling and error reporting in the domain.

---
+++ Ledger.Importer.Tests/Domain/Domain/ValueObjects/TransactionDateTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Ledger.Importer.Domain.Exceptions;
+using Ledger.Importer.Domain.ValueObjects;
+
+namespace Ledger.Importer.Tests.Domain.Domain.ValueObjects;
+
+public class TransactionDateTest
+{
+    [Fact]
+    public void TransactionDateIsCreatedSuccessfullyWithValidData()
+    {
+        var now = DateTime.UtcNow;
+        var transactionDate = new TransactionDate(now);
+        
+        transactionDate.Date.Should().Be(now);
+    }
+    
+    [Fact]
+    public void TransactionDateIsCreatedFromIsoFormat()
+    {
+        var date = TransactionDate.From("2025-05-10T12:45:00Z");
+        
+        date.Should().Be(new TransactionDate(new DateTime(2025, 05, 10, 12, 45, 00)));
+    }
+    
+    [Fact]
+    public void TransactionDateIsCreatedFromSimplifiedString()
+    {
+        var date = TransactionDate.From("2025-05-10");
+        
+        date.Should().Be(new TransactionDate(new DateTime(2025, 05, 10, 12, 45, 00)));
+    }
+    
+    [Fact]
+    public void ExceptionIsThrownWhenDatStringIsInvalid()
+    {
+        var transactionDateConstruction = () => TransactionDate.From("INVALID_DATE");
+        
+        transactionDateConstruction.Should().Throw<InvalidTransactionData>().WithMessage("Invalid date format: INVALID_DATE.");   
+    }
+    
+    [Fact]
+    public void ExceptionIsThrowWhenDateStringIsEmpty()
+    {
+        var transactionDateConstruction = () => TransactionDate.From("    ");
+        
+        transactionDateConstruction.Should().Throw<InvalidTransactionData>().WithMessage("Date cannot be empty.");
+    }
+}
