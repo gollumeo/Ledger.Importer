28b2adbd17bd26e68386bdca576c337f34acd6bd
Golluméo <p.mauriello54@gmail.com>
2025-05-26 16:06:20 +0200

    feat(importer): add handler for streaming transactions import
    Introduces `StreamTransactionsImport.ExecuteAsync` to handle streaming of transactions from a CSV file.
    Notifies the provided narrator of each transaction imported and import completion status.
    Supports parsing transactions line by line for efficiency.

---
+++ Ledger.Importer.Application/Handlers/StreamTransactionsImport.cs
@@ -0,0 +1,27 @@
+﻿using Ledger.Importer.Application.Commands;
+using Ledger.Importer.Application.Contracts;
+using Ledger.Importer.Domain.Entities;
+using Ledger.Importer.Domain.Services;
+
+namespace Ledger.Importer.Application.Handlers;
+
+public static class StreamTransactionsImport
+{
+    public static async Task ExecuteAsync(ImportTransactions command, INarrateTransactionsImportLive narrator)
+    {
+        using var reader = new StreamReader(command.Csv);
+        var header = await reader.ReadLineAsync();
+        
+        while(!reader.EndOfStream)
+        {
+            var line = await reader.ReadLineAsync();
+
+            if (line is not null && InterpretCsvAsTransactions.TryParseLine(line, out var transaction))
+            {
+                await narrator.NotifyTransactionImported(transaction);
+            }
+
+            await narrator.NotifyImportCompleted(2, 0);
+        }
+    }
+}
