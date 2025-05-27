aa07a3973d3897f2ecdf64af9c7705ff19000b8f
Golluméo <p.mauriello54@gmail.com>
2025-05-27 15:40:42 +0200

    feat(import): return detailed transaction import result
    Enhances import processing to return an `ImportedTransactions` object.
    Includes a list of transactions for better encapsulation and usability.
    Improves the handling of parsed CSV data.

--- Ledger.Importer.Application/Handlers/ProcessTransactionsImport.cs
+++ Ledger.Importer.Application/Handlers/ProcessTransactionsImport.cs
@@ -1,13 +1,17 @@
 ﻿using Ledger.Importer.Application.Commands;
-using Ledger.Importer.Domain.Entities;
+using Ledger.Importer.Application.ReadModels;
 using Ledger.Importer.Domain.Services;
 
 namespace Ledger.Importer.Application.Handlers;
 
 public static class ProcessTransactionsImport
 {
-   public static IEnumerable<Transaction> Execute(ImportTransactions command)
-   {
-       return command.Csv.Length == 0 ? [] : InterpretCsvAsTransactions.From(command.Csv);
-   }
+    public static ImportedTransactions Execute(ImportTransactions command)
+    {
+        var items = command.Csv.Length == 0
+            ? []
+            : InterpretCsvAsTransactions.From(command.Csv).ToList();
+
+        return new ImportedTransactions { Items = items };
+    }
 }
