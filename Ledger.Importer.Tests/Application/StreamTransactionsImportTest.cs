0d23f57efe3decce7ae7e08317021d89e51c8f56
Golluméo <p.mauriello54@gmail.com>
2025-05-27 11:33:36 +0200

    test(importer): add test for invalid CSV headers
    Introduces a unit test to ensure `StreamTransactionsImport` throws `InvalidCsvFormat` for invalid header formats. Validates error handling for improperly formatted CSVs.

--- Ledger.Importer.Tests/Application/StreamTransactionsImportTest.cs
+++ Ledger.Importer.Tests/Application/StreamTransactionsImportTest.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Ledger.Importer.Application.Commands;
 using Ledger.Importer.Application.Handlers;
+using Ledger.Importer.Domain.Exceptions;
 using Ledger.Importer.Tests.Fakes;
 
 namespace Ledger.Importer.Tests.Application;
@@ -26,5 +27,23 @@ public class StreamTransactionsImportTest
         narrator.Imported.Should().HaveCount(2);
         narrator.Failed.Should().BeEmpty();
         narrator.Completed.Should().Be((2, 0));       
+    }
+    
+    [Fact]
+    public void ThrowsIfHeadersAreInvalid()
+    {
+        const string csv = """
+                           desc,amnt,dt
+                           Uber Eats Paris,29.90,2025-05-13
+                           Uber Mons,200,2025-05-13
+                           """;
+        
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(csv));
+        var command = new ImportTransactions(stream);
+        var narrator = new FakeNarrateTransactionsImportLive();
+
+        var action = async () => await StreamTransactionsImport.ExecuteAsync(command, narrator);
+
+        action.Should().ThrowAsync<InvalidCsvFormat>();
     }
 }
